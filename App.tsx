f08b2aa4f323883f83f3b37f8790155f50050675
Alexandre Marques <alexandreacm.marques@gmail.com>
2025-09-20 00:20:22 -0300

    feat: removed all comments to test

--- App.tsx
+++ App.tsx
@@ -6,12 +6,6 @@ import { useColorScheme } from 'react-native';
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    //NEw COMMENT
-    //I am here....
-    //other thing bla bla bla
-    //new test to push the content to main branch
-    //feature-4
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar isDarkMode={isDarkMode} />
       <HomeScreen isDarkMode={isDarkMode} />
